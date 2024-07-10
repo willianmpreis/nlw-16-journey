640197a7da6a5851b9734d888bb2a1fac3f97a5e
Willian Reis <dev.wreis@gmail.com>
2024-07-09 22:30:51 -0300

    Adiciona controller para viagem

---
+++ nlw-journey-c-sharp/Journey/src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,21 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            var useCase = new RegisterTripUseCase();
+
+            useCase.Execute(request);
+
+            return Created(String.Empty, request);
+        }
+    }
+}
---
+++ nlw-journey-c-sharp/Journey/src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,16 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute(RequestRegisterTripJson request) 
+        {
+        }
+
+        private void Validate()
+        {
+
+        }
+    }
+}
